1d29700653b427b320ca1fb1d0b14367c4f67869
Yash <yashaqua@gmail.com>
2020-05-29 18:43:10 +0530

    :package: Updated dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React,{Component} from 'react';
 import {Cards,Chart, CountryPicker} from './components/index'
+import styles from './App.module.css'
 class App extends Component(){
   render(){
 
   
   return (
-    <div>
+    <div className={styles.container}>
       <Cards />
       <CountryPicker />
       <Chart />
