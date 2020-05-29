28bffbb444cce7b5d1a76a6a979f250a6dd1464e
Yash <yashaqua@gmail.com>
2020-05-29 18:37:02 +0530

    :sparkles: Added core components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React,{Component} from 'react';
-
+import {Cards,Chart, CountryPicker} from './components/index'
 class App extends Component(){
   render(){
-    
+
+  
+  return (
+    <div>
+      <Cards />
+      <CountryPicker />
+      <Chart />
+    </div>;
+  )
   }
-	return <div></div>;
 };
 
 export default App;
